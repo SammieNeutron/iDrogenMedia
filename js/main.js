2d3b9c307de5de78d58ec2c0c6dff9d8d9a13718
Samson Samuels <samsonadegboyega@gmail.com>
2018-08-21 13:27:15 +0100

    ..

--- js/main.js
+++ js/main.js
@@ -22,19 +22,19 @@ jQuery(document).ready(function( $ ) {
 
   $("#mission").on('click', function() {
     $("#about-title").html("<h2>OUR MISSION</h2>");
-    $("#story").html("Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of");
+    $("#story").html("To digitalise Africa business ideas.");
     $("#photo").attr('src', 'img/intro-carousel/2.jpg');
   });
 
   $("#vision").on('click', function() {
     $("#about-title").html("<h2>OUR VISION</h2>");
-    $("#story").html("There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.<br><br> All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.");
+    $("#story").html("To be the core instituion developing mobile and web solution for African SME.");
     $("#photo").attr('src', 'img/intro-carousel/3.jpg');
   });
 
   $("#values").on('click', function() {
     $("#about-title").html("<h2>COMPANY VALUES</h2>");
-    $("#story").html("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. <br><br>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?");
+    $("#story").html("We are passionate about creating brands, platforms, products, and experiences people love.");
     $("#photo").attr('src', 'img/intro-carousel/4.jpg');
   });
 
